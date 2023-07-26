90153bd2c7f4b5cc6002c73f5d88b1e63210385b
Patrizio Bekerle <patrizio@bekerle.com>
2023-07-26 13:09:23 +0200

    add: error class

---
+++ src/Helpers/Error.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dbp\Relay\BlobLibrary\Helpers;
+
+class Error extends \Exception
+{
+    private const WITHDETAILSSTATUS = -1;
+
+    public function __construct(?string $message = '', int $code = 0, \Throwable $previous = null)
+    {
+        if ($code === self::WITHDETAILSSTATUS) {
+            $decoded = json_decode($message, true, 512, JSON_THROW_ON_ERROR);
+            $code = $decoded['code'];
+            unset($decoded['code']);
+        } else {
+            $decoded = [
+                'message' => $message,
+                'errorId' => '',
+                'errorDetails' => null,
+            ];
+        }
+
+        parent::__construct($code, json_encode($decoded), $previous);
+    }
+
+    public static function withDetails(?string $message = '', array $errorDetails = [], int $code = 0): Error
+    {
+        $message = [
+            'code' => $code,
+            'message' => $message,
+            'errorDetails' => $errorDetails,
+        ];
+
+        return new Error(json_encode($message), self::WITHDETAILSSTATUS);
+    }
+}
